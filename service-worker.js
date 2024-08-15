ee5a32fb6af1912501cc0ed6edf5acf0647ea688
Abhijit Ghosh <abhijit722141@gmail.com>
2024-08-15 09:12:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
-self.addEventListener('fetch', function (event) {
-    // Intercept requests and reroute them
+self.addEventListener('fetch', function(event) {
     if (event.request.url.startsWith('https://abhijit-108.github.io/Dairy-App/proxy')) {
         const newUrl = event.request.url.replace('https://abhijit-108.github.io/Dairy-App/proxy', 'http://192.168.1.1');
-        event.respondWith(fetch(newUrl));
+        event.respondWith(
+            fetch(newUrl).catch(error => {
+                console.error('Fetch failed; returning offline page instead.', error);
+                return new Response('Offline or network error', {
+                    status: 404,
+                    statusText: 'Not Found',
+                });
+            })
+        );
     }
 });
