fb19b46eba0b88e779383d6fb410bae7f79f92cc
Abhijit Ghosh <abhijit722141@gmail.com>
2024-08-15 09:01:35 +0530

    let test

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function (event) {
     // Intercept requests and reroute them
-    if (event.request.url.startsWith('https://your-https-site.com/proxy')) {
-        const newUrl = event.request.url.replace('https://your-https-site.com/proxy', 'http://192.168.1.1');
+    if (event.request.url.startsWith('https://abhijit-108.github.io/Dairy-App/proxy')) {
+        const newUrl = event.request.url.replace('https://abhijit-108.github.io/Dairy-App/proxy', 'http://192.168.1.1');
         event.respondWith(fetch(newUrl));
     }
 });
